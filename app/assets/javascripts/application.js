85aa874f18fd08bd1220cbc7f9d0640723b6b99c
emmanu <emmanu.varghese@computingaustralia.group>
2021-03-17 11:40:57 +0530

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,23 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require jquery
+//= require fontawesome-free/js/all.min.js
+//= require bootstrap/js/bootstrap.bundle.js
+//= require jquery-easing/jquery.easing.js
+//= require js/sb-admin-2.js
+//= require datatables/jquery.dataTables.min.js
+//= require datatables/dataTables.bootstrap4.min.js
+//= require activestorage
+//= require turbolinks
+//= require_tree .
