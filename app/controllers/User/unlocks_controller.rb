7b664387cd2c167c423277bc466f45c4f4e03f6a
emmanu <emmanu.varghese@computingaustralia.group>
2021-05-14 15:45:26 +0530

    code glitch resolved

--- app/controllers/User/unlocks_controller.rb
+++ app/controllers/User/unlocks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-  class UnlocksController < Devise::UnlocksController
+  class User::UnlocksController < Devise::UnlocksController
     # GET /resource/unlock/new
     # def new
     #   super
